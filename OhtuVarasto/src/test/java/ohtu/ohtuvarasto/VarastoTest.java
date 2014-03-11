b968d1a3a6bd31b343d0adfc181819cc8a295bdd
Sami-Joonas Saarinen <sjsaarinen@gmail.com>
2014-03-11 21:06:10 +0200

    rikottu testi

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -84,7 +84,7 @@ public class VarastoTest {
         varasto.lisaaVarastoon(8);
         double saatuMaara = varasto.otaVarastosta(10);
         
-        assertEquals(8, saatuMaara, vertailuTarkkuus);
+        assertEquals(10, saatuMaara, vertailuTarkkuus);
     }
     
     @Test
